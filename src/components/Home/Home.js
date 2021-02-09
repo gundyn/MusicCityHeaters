4a4ce31c5dbebfcfcfd2773532c7a84d7348587d
gundyn <nathan.gundy@gmail.com>
2021-02-09 13:40:40 -0600

    Content: Home.js
    add information about the supporters group to home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => (
       <div className='col-sm-10 col-md-8 mx-auto mt-5'>
         <h1>Welcome!</h1>
         <br/>
-        <p>Information about the heaters</p>
+        <p>The Music City Heaters are a group for both new and experienced soccer fans who want to be a part of the Nashville SC supporters’ culture, but aren’t necessarily ready to lead the drum section. It’s true, we’ve let our percussion skills slip since middle school band, and we don’t deny that the Davidson County Fire Marshal revoked our smoke bomb license (IN AN EGREGIOUS VIOLATION OF DUE PROCESS), but we treat every NSC match like a party and we’re right there in The Backline, letting the refs hear it. So, if you’re interested in joining an awesome fellowship of guys and gals that loves NSC, but doesn’t take itself too seriously, come introduce yourself and we’ll get to know each other over a tall, crisp, refreshing Black Cherry White Claw (SPONSOR US).</p>
         <footer>
           <h2>Contect</h2>
           <br/>
