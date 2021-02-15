be0d65d2747de2a72a0c017da1a5c161c4ccd08a
gundyn <nathan.gundy@gmail.com>
2021-02-15 10:38:20 -0600

    Feat: Calendar
    added Heaters google calendar for posting events and important dates

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,8 +1,15 @@
 import React, { Fragment } from 'react'
+import Iframe from 'react-iframe'
 
 const Events = () => (
   <Fragment>
     <h1>Event Calendar goes here</h1>
+    <Iframe url="https://calendar.google.com/calendar/embed?src=musiccityheaters%40gmail.com&ctz=America%2FChicago"
+      width="100%"
+      height="500px"
+      display="initial"
+      position="relative"
+    />
   </Fragment>
 )
 
