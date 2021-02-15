7dc7ba9ef260750c962a6ea928fc818d7b157beb
gundyn <nathan.gundy@gmail.com>
2021-02-15 10:40:39 -0600

    Feat: Calendar
    Heater calendar added to events page
    with iframe

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -3,7 +3,7 @@ import Iframe from 'react-iframe'
 
 const Events = () => (
   <Fragment>
-    <h1>Event Calendar goes here</h1>
+    <h1>Music City Heaters Calendar</h1>
     <Iframe url="https://calendar.google.com/calendar/embed?src=musiccityheaters%40gmail.com&ctz=America%2FChicago"
       width="100%"
       height="500px"
