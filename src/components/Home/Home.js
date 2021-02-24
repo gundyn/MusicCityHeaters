9c128dc03b4f939b84b7894aca17bab3400f5560
gundyn <nathan.gundy@gmail.com>
2021-02-24 08:32:56 -0600

    Fix: Home.js
    connect spelling error fixed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,7 @@ const Home = () => (
         <br/>
         <p>The Music City Heaters are a group for both new and experienced soccer fans who want to be a part of the Nashville SC supporters’ culture, but aren’t necessarily ready to lead the drum section. It’s true, we’ve let our percussion skills slip since middle school band, and we don’t deny that the Davidson County Fire Marshal revoked our smoke bomb license (IN AN EGREGIOUS VIOLATION OF DUE PROCESS), but we treat every NSC match like a party and we’re right there in The Backline, letting the refs hear it. So, if you’re interested in joining an awesome fellowship of guys and gals that loves NSC, but doesn’t take itself too seriously, come introduce yourself and we’ll get to know each other over a tall, crisp, refreshing Black Cherry White Claw (SPONSOR US).</p>
         <footer>
-          <h2>Contect</h2>
+          <h2>Connect</h2>
           <br/>
           <div className="container">
             <div className="row justify-content-center text-center">
