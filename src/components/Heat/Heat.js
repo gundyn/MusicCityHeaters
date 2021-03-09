7d096b9726d3383ac80fd7e6078d686506c683fb
gundyn <nathan.gundy@gmail.com>
2021-03-09 14:08:55 -0600

    Feat; HEAT
    added a card for local distro

--- src/components/Heat/Heat.js
+++ src/components/Heat/Heat.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import { Card } from 'react-bootstrap'
 
 import heat from './heat.png'
 
@@ -20,6 +21,14 @@ const Heat = () => (
           </a>
         </div>
       </div>
+      <Card style={{ width: '25rem' }}>
+        <Card.Body>
+          <Card.Title className="text-dark text-center">Local Distro</Card.Title>
+          <Card.Text>The Local Distro is your new, go-to spot for groceries, a quick bite after work, a drink with friends, a play date for your pup, and that perfect, local-made housewarming gift. Swing by our take-out window while walking your dog, make new friends over a friendly game of cornhole, and bring the family out for a weekend bar-b-que brunch! We can’t wait to bring our community together and do great things!</Card.Text>
+          <Card.Link href="https://www.thelocaldistro.com/">Website</Card.Link>
+          <Card.Link href="https://g.page/The-Distro?share">Map</Card.Link>
+        </Card.Body>
+      </Card>
     </div>
   </Fragment>
 )
