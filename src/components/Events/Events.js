ca736832db01598765b557de7528ba220f1aca2b
gundyn <nathan.gundy@gmail.com>
2021-04-19 12:59:27 -0500

    cleaned up styling for iframe Calendar

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -6,12 +6,7 @@ import '../../index.scss'
 const Events = () => (
   <Fragment>
     <h1>Music City Heaters Calendar</h1>
-    <Iframe url="https://calendar.google.com/calendar/embed?src=musiccityheaters%40gmail.com&ctz=America%2FChicago"
-      width="100%"
-      height="500px"
-      display="initial"
-      position="relative"
-    />
+    <Iframe url="https://calendar.google.com/calendar/embed?src=musiccityheaters%40gmail.com&ctz=America%2FChicago"/>
     <Footer />
   </Fragment>
 )
