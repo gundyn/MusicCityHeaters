2ba2e6ba841c6ea2654f8fe0deaeaa03d07b602b
gundyn <nathan.gundy@gmail.com>
2021-04-26 13:22:46 -0500

    updated weather to look better

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -8,6 +8,7 @@ const Events = () => (
   <Fragment>
     <h1>Music City Heaters Calendar</h1>
     <Iframe url="https://calendar.google.com/calendar/embed?src=musiccityheaters%40gmail.com&ctz=America%2FChicago"/>
+    <br />
     <WeatherFetch />
     <Footer />
   </Fragment>
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,33 +1,31 @@
 
 import React, { useState, useEffect } from 'react'
+import '../../index.scss'
 
 function WeatherFetch () {
   const key = 'c1a0353f12095ca88e2d1e05adc0ac92'
-  const [feelsLike, setFeelsLike] = useState('')
   const [mainTemp, setMainTemp] = useState('')
   const [description, setDescription] = useState('')
   const [main, setMain] = useState('')
-  const [iconID, setIconID] = useState('')
   useEffect(() => {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Nashville&APPID=' + key + '&units=metric')
       .then(res => res.json())
       .then(data => {
         console.log(data)
-        setFeelsLike(data.main.feelsLike)
         setMainTemp(data.main.temp)
-        setDescription(data.weather[0].description)
+        setDescription(data.wind.speed)
         setMain(data.weather[0].main)
-        setIconID(data.weather[0].icon)
       })
   }, [])
+
   return (
-    <div>
-      <h1>Main Temperature : {mainTemp} Degrees Celsius</h1>
-      <h1>Feels like: {feelsLike} Degrees Celsius</h1>
-      <h1>Weather Parameter: {main}</h1>
-      <h1>Description : {description}</h1>
-      <img src={'http://openweathermap.org/img/wn/' + iconID + '@2x.png'}/>
+    <div className="weather">
+      <h1 className="weatherHeader">Your Weather</h1>
+      <h1>{mainTemp} °C</h1>
+      <h1>{main}</h1>
+      <h1>Wind : {description}</h1>
     </div>
   )
 }
+
 export default WeatherFetch
