598c3117ae3492668bf6d9d452ff73ed8808fc0c
gundyn <nathan.gundy@gmail.com>
2021-05-03 17:48:01 -0500

    working on a merch section

--- src/components/Merch/Cart.js
+++ src/components/Merch/Cart.js
@@ -2,7 +2,10 @@ import React, { Fragment } from 'react'
 
 const Cart = () => (
   <Fragment>
-    <h1></h1>
+    <h1>Your Cart</h1>
+    <h2></h2>
+    <img src="#"></img>
+    <p>need to add a way to add more to the cart or remove from cart</p>
   </Fragment>
 )
 
